8df35feddbd880334625688c74e92dd93890a065
MarkoVVV <sam.samlin@mail.ru>
2023-05-01 20:35:53 +0300

    fix 'offset'

--- src/Header/BurgerNav/BurgerNav.tsx
+++ src/Header/BurgerNav/BurgerNav.tsx
@@ -14,10 +14,10 @@ export const BurgerNav = () => {
     return (
         <div className={s.burgerNav}>
             <div className={menuIsOpen ? `${s.burgerNavItems} ${s.show}` : s.burgerNavItems}>
-                <Link activeClass={s.active} to="main" spy={true} smooth={true} offset={-100} duration={500}>
+                <Link activeClass={s.active} to="main" spy={true} smooth={true} offset={0} duration={500}>
                     Main
                 </Link>
-                <Link activeClass={s.active} to="skills" spy={true} smooth={true} offset={-100} duration={500}>
+                <Link activeClass={s.active} to="skills" spy={true} smooth={true} offset={0} duration={500}>
                     Skills
                 </Link>
                 <Link activeClass={s.active} to="projects" spy={true} smooth={true} offset={-100} duration={500}>
